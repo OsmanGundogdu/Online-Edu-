6aef09e15488c0f6d7d9d41aa6076a388a3e55f2
OsmanGundogdu54 <OsmanGundogdu@users.noreply.github.com>
2025-07-13 16:59:46 +0300

    Database connections handled and İnital migration created.

--- OnlineEdu.Business/obj/Debug/net8.0/OnlineEdu.Business.AssemblyInfo.cs
+++ OnlineEdu.Business/obj/Debug/net8.0/OnlineEdu.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEdu.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e39a52cf1c20338da04bb86b78ccd87a3f30b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEdu.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEdu.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
